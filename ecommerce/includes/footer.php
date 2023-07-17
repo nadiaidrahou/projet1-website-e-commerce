9511667ab6cde25083f850e5a876754e62a675dc
nadiaidrahou <nadiaidrahou03@gmail.com>
2023-07-17 15:51:30 +0100

    website Decor

---
+++ ecommerce/includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+     <CENTER> <strong>Copyright &copy; 2022 - NADIA IDRAHOU </strong></CENTER>
+    </div>
+</footer>

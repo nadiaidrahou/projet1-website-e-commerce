f0c9ef1be1b16391d294b1298b4bd2a2e4cb3fa3
nadiaidrahou <139008322+nadiaidrahou@users.noreply.github.com>
2023-07-17 18:13:47 +0100

    Delete ecommerce directory

--- ecommerce/includes/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer">
-    <div class="container">
-      <div class="pull-right hidden-xs">
-        <b>All rights reserved</b>
-      </div>
-     <CENTER> <strong>Copyright &copy; 2022 - NADIA IDRAHOU </strong></CENTER>
-    </div>
-</footer>
